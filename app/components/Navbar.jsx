eccb6dfc33359de36076f71f40cd3da500e0e934
VilliamTorstensson <135708823+VilliamTorstensson@users.noreply.github.com>
2023-12-04 16:37:17 +0100

    done

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { HiMenuAlt4 } from "react-icons/hi";
 import { MdOutlineClose } from "react-icons/md";
 import { TiArrowUpOutline } from "react-icons/ti";
+import { MdNavigation } from "react-icons/md";
 export default function Navbar () {
 
   const [isOpen, setIsOpen] = useState(false)
@@ -75,17 +76,21 @@ const handleClick2 = () => {
 
 
           <nav  className='md:hidden flex justify-center fixed bottom-[40px] inset-x-0 mx-auto z-40 w-full' >
-              <div onClick={handleClick2} className={`bg-white shadow-sm px-5 py-3 rounded-full text-black font-sans text-2xl font-bold ${isOpen2 ? 'w-80' : 'w-content'}`}>
-                <h1 className={`${isOpen2 ? 'hidden' : 'flex'}`} >N</h1>
-                <ul className={`w-full justify-between flex ${isOpen2 ? 'flex' : 'hidden'}`}>
+              <div onClick={handleClick2} className={`bg-white shadow-sm px-5 py-3 rounded-full text-black font-sans text-2xl font-bold ${isOpen2 ? 'w-content mx-2' : 'w-content'}`}>
+                <h1 className={`${isOpen2 ? 'hidden' : 'flex'}`} >
+                N
+                </h1>
+                <ul className={`w-full justify-center gap-1 flex ${isOpen2 ? 'flex' : 'hidden'}`}>
+                <Link className='text-sm px-2 py-1 rounded-full bg-black text-white' href="#nav">Top</Link>
                 <Link className='text-sm px-2 py-1 rounded-full bg-black text-white' href="#services">Services</Link>
-                <Link className='text-sm px-2 py-1 rounded-full bg-black text-white' href="#work">Work</Link>
                   <Link className='text-sm px-2 py-1 rounded-full bg-black text-white' href="#about">About</Link>
+                  <Link className='text-sm px-2 py-1 rounded-full bg-black text-white' href="#work">Work</Link>
+                 
                   
-                  
-                  <Link className='text-sm px-2 py-1 rounded-full bg-black text-white' href="#contact">Contact</Link>
+                  <Link className='animation-bouncy text-sm px-2 py-1 rounded-full bg-black text-white' href="#contact">Contact</Link>
                 </ul>
               </div>
+             
           </nav>
     </section>
   )
